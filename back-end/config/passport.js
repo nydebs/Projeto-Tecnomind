bcd6e43c38591cb948a43cda0fcd831a1c0e844d
Deborah Evelyn <nydeborahevelyn@gmail.com>
2025-11-12 21:22:23 -0400

    Configurando a URL local

--- back-end/config/passport.js
+++ back-end/config/passport.js
@@ -4,12 +4,12 @@ const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 // LINHA PARA IMPORTAR O CLIENTE PRISMA
 const prisma = require('./db'); 
-
+const callbackBaseURL = process.env.BASE_URL;
 module.exports = function(passport) {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: '/auth/google/callback'
+        callbackURL: `${callbackBaseURL}/auth/google/callback`
     },
     async (accessToken, refreshToken, profile, done) => {
         
